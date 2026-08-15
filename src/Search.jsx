b362935c1031a08dd98c2c95015594a75c0a2014
Jeevan <jeevank2782@gmail.com>
2026-08-15 23:04:24 +0530

    initial commit

---
+++ src/Search.jsx
@@ -0,0 +1,28 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from "react";
+export default  function Search(){
+    let [city, setCity] = useState("");
+
+    let handleChange = (evt) => {
+        setCity(evt.target.value);
+    }
+
+    let handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+    }
+
+    return(
+        <div className='Search'>
+            <h3>Search for weather</h3>
+            <form onSubmit={handleSubmit}>
+            <TextField id="city" label="City name" variant="outlined" required value={city} onChange={handleChange}/>
+            <br></br>
+            <br></br>
+            <Button variant="contained" type="submit">Search</Button>
+            </form>
+        </div>
+    );
+}
